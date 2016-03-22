406046d24f245c6ffc9c392b9ecad05f57ddaec7
Gar Lee <gardarr@gmail.com>
2016-03-22 12:56:34 -0700

    removed url

--- server/search/searchController.js
+++ server/search/searchController.js
@@ -1,7 +1,7 @@
 var Twitter = require('twitter');
 var twitterAPI = require('node-twitter-api');
 var Reddit = require('nraw');
-var Users = require('../users/userController.js');
+//var Users = require('../users/userController.js');
 //var userModel = require('../users/userModel.js');
 var moment = require('moment');
 var async = require('async');
@@ -11,14 +11,12 @@ var currentAccessSecret;
 var _requestToken;
 var _requestSecret;
 
-//client used for Twitter OAuth 2.0
 var client = new twitterAPI({
   consumerKey: process.env.TWITTER_CONSUMER_KEY,
   consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
   callback: process.env.TWITTER_CALLBACK
 })
-
-//User data used for fetching search results
+  
 var currentUser = new Twitter({
   consumerKey: process.env.TWITTER_CONSUMER_KEY,
   consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
@@ -50,8 +48,9 @@ var getAccess = function (req, res) {
   client.getAccessToken(_requestToken, _requestSecret, oauth_verifier, function (err, accessToken, accessTokenSecret, results) {
     if (err) {
       console.error("Error occured in TwitterController getAccess call");
-      res.send(err);
+      res.json(err);
     } else {
+      //access token & secret belong to individual user -- store into user db
       currentAccessToken = accessToken
       currentAccessSecret = accessTokenSecret
 
@@ -61,14 +60,14 @@ var getAccess = function (req, res) {
           access_token_key: accessToken,
           access_token_secret: accessTokenSecret
       })
-      res.redirect('http://localhost:3000/home')
+      res.redirect('http://localhost:3000/home');
     }
   });
 };
 
 //* getData is the handler for client submit
 var getData = function (req, res) {
-
+  
   //split the search query into words
   var searchInfo = req.body.input.split(' ');
   //parse the array to remove any non alpha-numeric characters
@@ -136,20 +135,6 @@ var getData = function (req, res) {
     callback(null, parsedTweets);
   };
 
-  //Randomly shuffle the array of total results to give a mix of tweets and reddits
-  var randomizeResults = function (results, callback) {
-    var i = 0;
-    var j = 0;
-    var z = null;
-    for (var i = results.length - 1; i > 0; i -=1) {
-      j = Math.floor(Math.random () * (i+1));
-      z = results[i];
-      results[i] = results[j];
-      results[j] = z;
-    }
-    callback(null, results);
-  };
-
   var gatherParsedReddits = function (searchInfo, callback) {
     async.waterfall([
       async.apply(gatherReddits, searchInfo),
@@ -176,6 +161,20 @@ var getData = function (req, res) {
       });
   };
 
+  //Randomly shuffle the array of total results to give a mix of tweets and reddits
+  var randomizeResults = function (results) {
+    var i = 0;
+    var j = 0;
+    var z = null;
+    for (var i = results.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i+1));
+      z = results[i];
+      results[i] = results[j];
+      results[j] = z;
+    }
+    return results;
+  };
+
   async.parallel([
     function (callback) {
       gatherParsedTweets(searchInfo, callback)
@@ -187,29 +186,13 @@ var getData = function (req, res) {
       if (err) {
         console.error('An error occured in async parallel ', err);
       }
-      async.applyEach([randomizeResults], results, function (err, data) {
-        console.log('data: ', data)
-        res.json(data);
-      })
+      var shuffledData = randomizeResults(results);
+      res.json(shuffledData);
+      // async.applyEach([randomizeResults], results, function (err, data) {
+      //   console.log('data: ', data)
+      //   res.json(data);
+      // })
     });
-
-
-
-  //Asynchronously execute all the functions in order to generate JSON results
-  // async.waterfall([
-  //   async.apply(parseString, searchInfo),
-  //   gatherReddits,
-  //   gatherTweets,
-  //   parseTweets,
-  //   parseReddits,
-  //   randomizeResults 
-  //   ], function (err, result) {
-  //     if (err) {
-  //       console.error('An error occured in async waterfall', err);
-  //       return res.json(err);
-  //     }
-  //     return res.json(result);
-  //   });
 
 };
 
